8c33bf0a1da4a113b39a928fc1dd27a69a365f23
David Rolfs and Halle Tempest <halle@>
2017-04-26 16:16:56 -0700

    use javascript and jquery to return nput values

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
+  $("form#survey").submit(function(event){
+    event.preventDefault()
+    var name =$("#name").val();
+    var food = $("#food").val();
+    var music= $("input:radio[name=music]:checked").val();
+    var date= $("#date").val();
+    var favoriteColor = $("#color").val();
 
-
-
-
-
-
-
-
-
+    $("#display").text("Your name is " + name + " favorite food is " + food + ", favorite music is " + music + " todays date is " + date + " favorite color hex is " + favoriteColor);
 
 
   });
+});
